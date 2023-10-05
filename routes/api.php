7d54067eee29ede41259a6ee65e598bb1a5c14cd
yussufmussa <alphillipsa@gmail.com>
2023-10-05 15:17:43 +0300

    Password Update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\LogoutController;
+use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
 use App\Http\Controllers\Api\V1\Auth\ProfileController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
@@ -22,9 +24,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('auth/register', RegisterController::class);
 Route::post('auth/login', LoginController::class);
+Route::post('auth/logout', LogoutController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [ProfileController::class, 'show']);
     Route::put('profile' , [ProfileController::class,  'update']);
+    Route::put('password', PasswordUpdateController::class);
 });
 
